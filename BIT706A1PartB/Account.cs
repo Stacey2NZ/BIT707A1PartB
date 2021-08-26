ae5951d7a88359a9bd94388ea95f2e6559824065
stacey watson <staceywatson1987@gmail.com>
2021-08-26 19:10:21 +1200

    Add project files.

---
+++ BIT706A1PartB/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIT706A1PartB
+{
+    class Account
+    {
+        private double balance;
+        private string accountName;
+        private double depositAmount;
+        private double withdraAmount;
+        private int accountID;
+        private double interestRate;
+        private double failFee;
+
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+    }
+}
